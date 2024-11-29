aa6f8eb85238b9daac4d4b33925b9528dcd02c54
Twig <littleb88888888@gmail.com>
2024-11-29 16:39:02 -0700

    Sketched character presenter - added script to manage player and opponent character presentation - moved throw manager location object into prefab - moved character sprites into character presenter branch

---
+++ Assets/Scripts/Battle System/Presenters/CharacterPresenter.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CharacterPresenter: Tweener
+{
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _opponentTransform;
+
+    [Header("Swap Animation")]
+    [SerializeField] private Vector2 _inactiveOffset = new(-10, -1);
+    [SerializeField] private float _tweenPeriod = 0.5f;
+    [SerializeField] private TransitionType _swapTransition = TransitionType.Back;
+    [SerializeField] private EaseType _swapEasing = EaseType.EaseOut;
+
+
+    private void Start()
+    {
+        _playerTransform.localPosition = _inactiveOffset;
+        _opponentTransform.localPosition = _inactiveOffset;
+
+        if (TurnManager.IsPlayerTurn) SwapPlayerIn(); else SwapOpponentIn();
+
+        BattleEventBus.PlayerTurnStarted += SwapPlayerIn;
+        BattleEventBus.OpponentTurnStarted += SwapOpponentIn;
+    }
+
+    private void OnDestroy()
+    {
+        BattleEventBus.PlayerTurnStarted -= SwapPlayerIn;
+        BattleEventBus.OpponentTurnStarted -= SwapOpponentIn;
+    }
+
+
+    private void SwapPlayerIn() => StartSwapTween(_playerTransform, _opponentTransform);
+    private void SwapOpponentIn() => StartSwapTween(_opponentTransform, _playerTransform);
+
+    private void StartSwapTween(Transform activeCharacter, Transform inactiveCharacter)
+    {
+        StartPositionTween(activeCharacter, Vector2.zero, _tweenPeriod, _swapTransition, _swapEasing);
+        StartPositionTween(inactiveCharacter, _inactiveOffset, _tweenPeriod, _swapTransition, _swapEasing);
+    }
+}
