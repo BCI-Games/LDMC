40113dbdce92ec0a50ae6a4266cfaebd3f1117f0
Twig <littleb88888888@gmail.com>
2024-11-30 00:34:31 -0700

    Fixed tween lerping - changed lerping functions to use unclamped variations to work with unclamped easing methods

--- Assets/Scripts/Visuals/Lerper.cs
+++ Assets/Scripts/Visuals/Lerper.cs
@@ -7,9 +7,9 @@ public abstract class Lerper: MonoBehaviour
         Debug.LogWarning("Lerp Method not defined for type " + typeof(TValue));
         return a;
     }
-    protected float Lerp(float a, float b, float t) => Mathf.Lerp(a, b, t);
-    protected Color Lerp(Color a, Color b, float t) => Color.Lerp(a, b, t);
-    protected Vector2 Lerp(Vector2 a, Vector2 b, float t) => Vector2.Lerp(a, b, t);
-    protected Vector3 Lerp(Vector3 a, Vector3 b, float t) => Vector3.Lerp(a, b, t);
-    protected Quaternion Lerp(Quaternion a, Quaternion b, float t) => Quaternion.Lerp(a, b, t);
+    protected float Lerp(float a, float b, float t) => Mathf.LerpUnclamped(a, b, t);
+    protected Color Lerp(Color a, Color b, float t) => Color.LerpUnclamped(a, b, t);
+    protected Vector2 Lerp(Vector2 a, Vector2 b, float t) => Vector2.LerpUnclamped(a, b, t);
+    protected Vector3 Lerp(Vector3 a, Vector3 b, float t) => Vector3.LerpUnclamped(a, b, t);
+    protected Quaternion Lerp(Quaternion a, Quaternion b, float t) => Quaternion.LerpUnclamped(a, b, t);
 }
