80f89f69d818499d8f913e8e56bc10a42cf7fd5c
Twig <littleb88888888@gmail.com>
2025-02-12 15:27:02 -0700

    Sketched BCI2000 controller - implemented draft of a featured bci 2000 controller based on available support - added helper data classes to streamline initialization parameters

---
+++ Assets/Scripts/BCI Integration/BCI2000/BCI2000Controller.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using BCI2000RemoteNET;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BCI2000 
+{
+    using static BCI2000Remote;
+    public class BCI2000Controller: MonoBehaviour
+    {
+        [SerializeField] private bool _autoConnect = true;
+        [SerializeField] private bool _startWhenConnected = true;
+        [SerializeField] private bool _stopWithScene = true;
+        [SerializeField] private bool _autoDisconnect = true;
+        
+
+        [Header("Operator")]
+        [SerializeField] private bool _startLocalOperator = true;
+        [SerializeField] private string _operatorPath;
+        [SerializeField] private string _operatorAddress = "127.0.0.1";
+        [SerializeField] private int _operatorPort = 3999;
+
+
+        [Header("Modules")]
+        [SerializeField] private bool _startModulesWithConnection = true;
+        [SerializeField] private BCI2000Module[] _startupModules
+        = new BCI2000Module[] {
+            new("SignalGenerater"),
+            new("DummySignalProcessing"),
+            new("DummyApplication")
+        };
+
+
+        [Header("Events")]
+        [SerializeField] private BCI2000EventDefinition[] _events;
+
+        [Header("Parameters")]
+        [SerializeField] private BCI2000ParameterDefinition[] _parameters;
+        [SerializeField] private string[] _parameterFiles;
+
+        [Header("States")]
+        [SerializeField] private BCI2000StateDefinition[] _states;
+
+
+        private BCI2000Remote _remote;
+
+
+        void Awake()
+        {
+            if (!_autoConnect) return;
+
+            if (_startLocalOperator)
+                StartAndConnectToLocalOperator(_operatorPath);
+            else
+                ConnectToOperator(_operatorPort, _operatorAddress);
+        }
+
+        void OnDestroy()
+        {
+            if (_stopWithScene)
+                _remote?.Stop();
+
+            if (_autoDisconnect)
+                _remote?.connection.Quit();
+        }
+
+
+        public void StartAndConnectToLocalOperator
+        (
+            string operatorPath, int port = 3999
+        )
+        {
+            if (!File.Exists(_operatorPath))
+            {
+                Debug.LogWarning("Operator path invalid, aborting...");
+                return;
+            }
+            BCI2000Connection connection = new();
+            connection.StartOperator(operatorPath, port: port);
+            ConnectToOperator(port, existingConnection: connection);
+        }
+
+        public void ConnectToOperator
+        (
+            int port = 3999, string address = "127.0.0.1",
+            BCI2000Connection existingConnection = null
+        )
+        {
+            BCI2000Connection connection = existingConnection ?? new();
+            connection.Connect(address, port);
+            _remote = new(connection);
+
+            AddEvents(_events);
+            AddStates(_states);
+            AddParameters(_parameters);
+
+            if (_startModulesWithConnection)
+            {
+                StartCoroutine
+                (
+                    RunAwaitSystemState
+                    (
+                        SystemState.Connected, CompleteInitialization
+                    )
+                );
+            }
+            else
+            {
+                CompleteInitialization();
+            }
+        }
+
+        protected void CompleteInitialization()
+        {
+            LoadParameterFiles(_parameterFiles);
+            if (_startWhenConnected)
+                _remote.Start();
+        }
+
+
+        public void StartModules(BCI2000Module[] modules)
+        {
+            Dictionary<string, IEnumerable<string>> moduleDictionary = new();
+            foreach(var (name, arguments) in modules)
+                moduleDictionary.Add(name, arguments);
+            _remote.StartupModules(moduleDictionary);
+        }
+
+        public void AddEvents(BCI2000EventDefinition[] events)
+        {
+            foreach(var (name, bitWidth, initialValue) in events)
+                _remote.AddEvent(name, bitWidth, initialValue);
+        }
+
+        public void AddStates(BCI2000StateDefinition[] states)
+        {
+            foreach(var (name, bitWidth, initialValue) in states)
+                _remote.AddEvent(name, bitWidth, initialValue);
+        }
+
+        public void AddParameters(BCI2000ParameterDefinition[] parameters)
+        {
+            foreach
+            (
+                var (
+                    section, name, defaultValue,
+                    minimumValue, maximumValue
+                ) in parameters
+            )
+            {
+                _remote.AddParameter
+                (
+                    section, name, defaultValue,
+                    minimumValue, maximumValue
+                );
+            }
+        }
+
+        public void LoadParameterFiles(string[] filePaths)
+        {
+            foreach(string path in filePaths)
+            {
+                if (File.Exists(path))
+                    _remote.LoadParameters(path);
+                else
+                    Debug.LogWarning("Parameter file not found: "  +path);
+            }
+        }
+
+
+        protected IEnumerator RunAwaitSystemState
+        (
+            SystemState state, Action callback,
+            float pollingPeriod = 0.1f
+        )
+        {
+            while(!_remote.WaitForSystemState(state, 0))
+            {
+                yield return new WaitForSeconds(pollingPeriod);
+            }
+            callback();
+        }
+    }
+}
