1d73632b4ba9826736d357d0b50dcae1ca266734
Twig <littleb88888888@gmail.com>
2025-03-24 17:17:42 -0600

    Fixed training windows - reworked training script to continuously send multiple training markers with a common window length - changed target indexes from 0 and 1 to 1 and 2

--- Assets/Scripts/BCI Integration/BCI Essentials/BlockTrainBCIEssentialsTrainer.cs
+++ Assets/Scripts/BCI Integration/BCI Essentials/BlockTrainBCIEssentialsTrainer.cs
@@ -1,48 +1,77 @@
+using System.Collections;
 using UnityEngine;
 
 public class BlockTrainBCIEssentialsTrainer: MonoBehaviour
 {
     private bool _isInTrial;
+    private float _windowLength;
     private bool _shouldUpdateClassifier;
+
+    private bool IsTraining => _markerRoutine != null;
+    private Coroutine _markerRoutine = null;
+
 
     void Start()
     {
-        BlockTrainConductor.OffBlockStarted += SendOffBlockMarker;
-        BattleEventBus.WindupStarted += SendOnBlockMarker;
+        _windowLength = GetCommonWindowLength();
+
+        BlockTrainConductor.OffBlockStarted += SendOffBlockMarkers;
+        BattleEventBus.WindupStarted += SendActiveWindowMarkers;
         BattleEventBus.MonsterCaptured += OnMonsterCaptured;
     }
 
     void OnDestroy()
     {
-        BlockTrainConductor.OffBlockStarted -= SendOffBlockMarker;
-        BattleEventBus.WindupStarted -= SendOnBlockMarker;
+        BlockTrainConductor.OffBlockStarted -= SendOffBlockMarkers;
+        BattleEventBus.WindupStarted -= SendActiveWindowMarkers;
         BattleEventBus.MonsterCaptured -= OnMonsterCaptured;
 
         if (_isInTrial) SendTrialEndsMarker();
+        if (IsTraining) StopCoroutine(_markerRoutine);
         if (_shouldUpdateClassifier) SendTrainingCompleteMarker();
     }
 
 
-    void SendOffBlockMarker()
+    void SendOffBlockMarkers()
     {
         if (!_isInTrial)
         {
             _isInTrial = true;
             SendTrialStartedMarker();
         }
-        if (_shouldUpdateClassifier) SendUpdateClassifierMarker();
-        SendMIEventMarker(Settings.OffBlockDuration, 0);
+
+        SendMarkersForPeriod(1, Settings.OffBlockDuration);
     }
 
-    void SendOnBlockMarker()
+    void SendActiveWindowMarkers()
     {
-        if (_shouldUpdateClassifier) SendUpdateClassifierMarker();
-        SendMIEventMarker(Settings.CharacterActiveDuration, 1);
+        SendMarkersForPeriod(2, Settings.CharacterActiveDuration);
     }
 
     void OnMonsterCaptured(MonsterData _)
     {
-        _shouldUpdateClassifier = true;
+        SendUpdateClassifierMarker();
+    }
+
+
+    private void SendMarkersForPeriod
+    (int trainingTarget, float duration)
+    => _markerRoutine = StartCoroutine
+    (
+        RunSendMarkersForPeriod(
+            trainingTarget, duration
+        )
+    );
+    IEnumerator RunSendMarkersForPeriod
+    (int trainingTarget, float duration)
+    {
+        float lifetime = 0;
+        while(lifetime < duration)
+        {
+            SendMIEventMarker(_windowLength, trainingTarget);
+            yield return new WaitForSeconds(_windowLength);
+            lifetime += _windowLength;
+        }
     }
 
 
@@ -59,5 +88,23 @@ public class BlockTrainBCIEssentialsTrainer: MonoBehaviour
     void WriteMarker(string markerString)
     {
         PersistentMarkerStream.PushString(markerString);
+    }
+
+    
+    public static float GetCommonWindowLength()
+    => GetCommonDivisor(Settings.CharacterActiveDuration, Settings.OffBlockDuration);
+
+    public static float GetCommonDivisor(float a, float b)
+    {
+        if (a == b)
+            return a;
+
+        float max = Mathf.Max(a, b);
+        float min = Mathf.Min(a, b);
+
+        if (max % min == 0) return min;
+        if (1 / min % max == 0) return 1 / min;
+        if (1 / max % min == 0) return 1 / max;
+        return 1 / (min * max);
     }
 }
