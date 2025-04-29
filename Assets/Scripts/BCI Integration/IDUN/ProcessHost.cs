2d9b68109c8de83c3f1efa7e117a17cde3aa4473
Twig <littleb88888888@gmail.com>
2025-04-29 09:54:20 -0600

    Sketched process host - created basic script to launch and manage a process from streaming assets

---
+++ Assets/Scripts/BCI Integration/IDUN/ProcessHost.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+
+using Debug = UnityEngine.Debug;
+using UnityEditor.EditorTools;
+
+public class ProcessManager: MonoBehaviour
+{
+    public event DataReceivedEventHandler OutputDataReceived;
+    public event DataReceivedEventHandler ErrorDataReceived;
+
+
+    [Tooltip("filepath of the target application relative to the streaming assets folder")]
+    public string ApplicationPath;
+    public string[] Arguments;
+
+    public bool ProcessRunning => _process != null && !_process.HasExited;
+    private Process _process;
+
+
+    void Start()
+    {
+        OutputDataReceived += (sender, args) => {
+            Debug.Log("process Data received: " + args.Data);
+        };
+        StartProcess();
+    }
+
+    void OnDestroy()
+    {
+        if (ProcessRunning) EndProcess();
+    }
+
+    public void StartProcess()
+    {
+        string fullApplicationPath = Path.Combine(Application.streamingAssetsPath, ApplicationPath);
+        if (!File.Exists(fullApplicationPath))
+        {
+            Debug.LogWarning("Target application not found");
+            return;
+        }
+        _process = Process.Start(fullApplicationPath, string.Join(' ', Arguments));
+        _process.OutputDataReceived += BindDataHandler(OutputDataReceived);
+        _process.ErrorDataReceived += BindDataHandler(ErrorDataReceived);
+    }
+
+    public void EndProcess()
+    {
+        if (!ProcessRunning)
+        {
+            Debug.LogWarning("Process isn't running");
+        }
+        _process.Kill();
+    }
+
+    private DataReceivedEventHandler BindDataHandler(DataReceivedEventHandler dataEvent)
+    => (sender, dataArgs) => dataEvent?.Invoke(sender, dataArgs);
+}
