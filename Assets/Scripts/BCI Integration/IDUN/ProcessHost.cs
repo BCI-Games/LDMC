eab0d78530a58c7ed1f47ecadfba1f2bfff1e815
Twig <littleb88888888@gmail.com>
2025-04-30 14:53:03 -0600

    Fixed process host class name

--- Assets/Scripts/BCI Integration/IDUN/ProcessHost.cs
+++ Assets/Scripts/BCI Integration/IDUN/ProcessHost.cs
@@ -6,7 +6,7 @@ using System;
 using Debug = UnityEngine.Debug;
 
 [ExecuteAlways]
-public class ProcessManager: MonoBehaviour
+public class ProcessHost: MonoBehaviour
 {
     public event Action<string> OutputLineReceived;
     public event Action<string> ErrorLineReceived;
