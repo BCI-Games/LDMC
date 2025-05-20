45c7fa367779b9dd7cac2fc4525787b645fb0ab4
Twig <littleb88888888@gmail.com>
2025-05-20 14:06:02 -0600

    Implemented celebration animation - added celebration animation to player animator state machine - implemented monster novelty check and celebration animation in animated character presenter

--- Assets/Scripts/Battle System/Presenters/AnimatedCharacterPresenter.cs
+++ Assets/Scripts/Battle System/Presenters/AnimatedCharacterPresenter.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Animator))]
-public class AnimatedCharacterPresenter: CharacterPresenter
+public class AnimatedCharacterPresenter : CharacterPresenter
 {
     [SerializeField] protected Animator _sleepyZedAnimator;
 
     private Animator _animator;
+    private readonly List<MonsterData> _discoveredMonsters = new();
+    private bool _currentMonsterIsNew;
 
 
     protected override void Start()
@@ -19,6 +22,13 @@ public class AnimatedCharacterPresenter: CharacterPresenter
         _animator = GetComponent<Animator>();
 
         SubscribeToBattleEvents();
+        BattleEventBus.MonsterAppeared += CheckMonsterNovelty;
+    }
+
+    protected override void OnDestroy()
+    {
+        UnsubscribeFromBattleEvents();
+        BattleEventBus.MonsterAppeared -= CheckMonsterNovelty;
     }
 
 
@@ -39,9 +49,25 @@ public class AnimatedCharacterPresenter: CharacterPresenter
 
     protected override void ShowRestStarted()
     {
+        if (!_currentMonsterIsNew) StartRestAnimation();
+        else StartCoroutine(RunCelebrationDisplay());
+    }
+
+    private void StartRestAnimation()
+    {
         _animator.SetBool("Resting", true);
+        _animator.ResetTrigger("Snooze");
         _sleepyZedAnimator.gameObject.SetActive(true);
         EmitSleepyZed();
+    }
+
+    private IEnumerator RunCelebrationDisplay()
+    {
+        _animator.Play("Celebrate");
+        yield return new WaitForSeconds(
+            _animator.GetCurrentAnimatorStateInfo(0).length / 2
+        );
+        StartRestAnimation();
     }
 
     protected override IEnumerator RunRestCycle()
@@ -50,6 +76,13 @@ public class AnimatedCharacterPresenter: CharacterPresenter
         _animator.SetTrigger("Snooze");
         EmitSleepyZed();
     }
-    
+
     protected void EmitSleepyZed() => _sleepyZedAnimator.SetTrigger("Emit");
+    
+    
+    private void CheckMonsterNovelty(MonsterData newMonster)
+    {
+        _currentMonsterIsNew = !_discoveredMonsters.Contains(newMonster);
+        if (_currentMonsterIsNew) _discoveredMonsters.Add(newMonster);
+    }
 }
