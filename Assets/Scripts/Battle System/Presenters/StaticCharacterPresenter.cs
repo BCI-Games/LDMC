8f4f2f65e829c3ed489a17d03b01598fdf116e28
Twig <littleb88888888@gmail.com>
2025-06-17 11:49:15 -0600

    Fixed simplified animation positioning - added sprite offsets to static character presenter to work with updated sprites

--- Assets/Scripts/Battle System/Presenters/StaticCharacterPresenter.cs
+++ Assets/Scripts/Battle System/Presenters/StaticCharacterPresenter.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(SpriteRenderer))]
-public class StaticCharacterPresenter: CharacterPresenter
+public class StaticCharacterPresenter : CharacterPresenter
 {
     [SerializeField] private GameObject _sleepyZObject;
 
     [Header("Sprites")]
-    [SerializeField] private Sprite _idleSprite;
-    [SerializeField] private Sprite _activeSprite;
-    [SerializeField] private Sprite _restSprite;
-    
+    [SerializeField] private SpriteWithOffset _idleSprite;
+    [SerializeField] private SpriteWithOffset _activeSprite;
+    [SerializeField] private SpriteWithOffset _restSprite;
+
     private SpriteRenderer _renderer;
     private bool _isResting;
 
@@ -35,23 +35,29 @@ public class StaticCharacterPresenter: CharacterPresenter
     protected override void ShowThrow()
         => SetSpriteIfNotResting(_idleSprite);
 
-    private void SetSpriteIfNotResting(Sprite newSprite)
+    private void SetSpriteIfNotResting(SpriteWithOffset newSprite)
     {
         if (_isResting) return;
-        _renderer.sprite = newSprite;
+        SetSprite(newSprite);
+    }
+
+    private void SetSprite(SpriteWithOffset newSprite)
+    {
+        _renderer.sprite = newSprite.Sprite;
+        transform.localPosition = newSprite.Offset;
     }
 
     protected override void ShowRestEnded()
     {
         _isResting = false;
-        _renderer.sprite = _idleSprite;
+        SetSprite(_idleSprite);
         _sleepyZObject.SetActive(false);
     }
-    
+
     protected override void ShowRestStarted()
     {
         _isResting = true;
-        _renderer.sprite = _restSprite;
+        SetSprite(_restSprite);
     }
 
     protected override IEnumerator RunRestCycle()
@@ -60,5 +66,12 @@ public class StaticCharacterPresenter: CharacterPresenter
         yield return new WaitForSeconds(Settings.CharacterActiveDuration);
         _sleepyZObject.SetActive(false);
         yield return new WaitForSeconds(Settings.CharacterIdleDuration);
+    }
+
+    [System.Serializable]
+    public class SpriteWithOffset
+    {
+        public Sprite Sprite;
+        public Vector2 Offset = Vector2.zero;
     }
 }
