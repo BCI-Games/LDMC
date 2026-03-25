52e1490cb7757f87a21b89150589e0bb0974a511
Twig <git@twig.gay>
2026-03-25 09:42:21 -0600

    Fixed resting state bug in character presenter

--- Assets/Scripts/Battle System/Presenters/CharacterPresenter.cs
+++ Assets/Scripts/Battle System/Presenters/CharacterPresenter.cs
@@ -42,11 +42,13 @@ public abstract class CharacterPresenter: MonoBehaviour
     private void StartRestCycle()
     {
         ShowRestStarted();
+        if (_restCycleCoroutine != null) StopCoroutine(_restCycleCoroutine);
         _restCycleCoroutine = StartCoroutine(WrapRestCycle());
     }
     private void EndRestCycle()
     {
         StopCoroutine(_restCycleCoroutine);
+        _restCycleCoroutine = null;
         ShowRestEnded();
     }
     private IEnumerator WrapRestCycle()
